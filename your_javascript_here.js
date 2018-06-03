9aaab0bca4c3cbc57f4e0d5c494be5184e723e53
Jelename <yelena.medenica@gmail.com>
2018-06-03 17:22:36 +0200

    javaScript changes

--- your_javascript_here.js
+++ your_javascript_here.js
@@ -58,26 +58,53 @@ function doBattle(heroicCreature, creature) {
 }
 
 // UI
+let bedPic = document.getElementById('bed');
+bedPic.onclick = function() {rest(hero)};
+
+let weaponPic = document.getElementById('weapon');
+weaponPic.onclick = function() {pickUpItem(hero, {type: 'Water gun', damage: 4})};
+
+let enemyPic = document.getElementById('enemy');
+enemyPic.onclick = function() {doBattle(hero, {health: 8, weapon: {damage: 2}})};
+
+let takeWeaponPic = document.getElementById('takeWeapon');
+takeWeaponPic.onclick = function() {equipWeapon(hero, window.prompt())};
+
+// function displayStats() {
+//   let heroStats = document.createTextNode(`Your superhero name is: ${hero.name}, Your superhero health is: ${hero.health}, Your superhero weapon type is: ${hero.weapon.type} with the damage value of: ${hero.weapon.damage}`);
+//   let statsHolder = document.getElementById('stats');
+//   statsHolder.appendChild(heroStats);
+// }
+
 function displayStats() {
-  let heroStats = document.createTextNode(`Your superhero name is: ${hero.name}, Your superhero health is ${hero.health}, Your superhero weapon type is: ${hero.weapon.type} with the damage value of: ${hero.weapon.damage}`)
-  let statsHolder = document.getElementById('stats');
-  statsHolder.appendChild(heroStats);
+  //name
+  let heroName = document.getElementById('heroName');
+  heroName.innerText = `Your superhero name is: ${hero.name}`
 
-  // let heroStats = document.getElementById('stats');
-  // let heroItem = document.createElement('li');
-  // let choosingItems = hero.map(function(item) )
+  //health
+  let heroHealthItem = document.getElementById('heroHealt');
+  heroHealthItem.innerText = `Your superhero health level is: ${hero.health}`;
 
+  //weapon
+  let heroWeaponItem = document.getElementById('heroWeapon');
+  heroWeaponItem.innerText = `Your superhero weapon is: ${hero.weapon.type}, with a level damage of: ${hero.weapon.damage}`
 
 }
 
+
+//Section 4
 function displayInventory() {
-  hero.inventory.forEach(function(items) {
-    let item = document.createElement('li');
-    item.append(items);
-    let inventoryListElement = document.getElementById('inventoryList');
-    inventoryListElement.appendChild(item);
-  })
+  let inventoryListElement = document.getElementById('inventoryList');
+  for(let i = 0; i < hero.inventory.length; i++) {
+    let listItem = document.createElement('li');
+    let listItemType = hero.inventory[i].type;
+    let listItemDamage = hero.inventory[i].damage;
+    listItem.innerText = `Type: ${listItemType}, Damage: ${listItemDamage}`;
+                        //'type: ' + listItemType + ", Damage" + listItemDamage;
+    inventoryListElement.appendChild(listItem);
+  }
 }
+
 
 
 
